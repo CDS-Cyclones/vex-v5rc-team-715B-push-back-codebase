cc39507b26f6f05ce1b85ba61212d64f55344e73
marcuskongjika <119444616+interluded@users.noreply.github.com>
2025-09-18 16:21:58 -0400

    Inital Commit

---
+++ include/robot-config.h
@@ -0,0 +1,21 @@
+using namespace vex;
+
+extern brain Brain;
+extern motor backLeftMotor;
+extern motor backRightMotor;
+extern motor backRightMotor;
+extern motor frontRightMotor;
+
+
+extern motor_group LeftDriveSmart;
+extern motor_group RightDriveSmart;
+
+extern drivetrain Drivetrain;
+extern controller Controller;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
---
+++ src/robot-config.cpp
@@ -0,0 +1,54 @@
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of brain used for printing to the V5 brain screen
+brain Brain;
+
+
+const distanceUnits DISTANCE_UNITS = mm;
+
+const gearSetting DRIVETRAIN_MOTOR_GEAR_RATIO = ratio18_1;
+const double WHEEL_CIRCUMFERENCE = 3.25 * atan(1) * 4;
+
+const double TRACKWIDTH = 351.6;
+const double INTERNAL_GEAR_RATIO = 1;
+const double WHEELBASE = 269.24;
+
+
+//DRIVETRAIN PORT DEFINITIONS
+const int32_t BACK_LEFT_MOTOR_PORT = 1;
+const int32_t FRONT_LEFT_MOTOR_PORT = 2;
+const int32_t BACK_RIGHT_MOTOR_PORT = 3;
+const int32_t FRONT_RIGHT_MOTOR_PORT = 4;
+
+
+//DRIVETRAIN INVERSIONS
+const bool BACK_LEFT_MOTOR_INVERTED = true;
+const bool BACK_RIGHT_MOTOR_INVERTED = true;
+const bool FRONT_LEFT_MOTOR_INVERTED = false;
+const bool FRONT_RIGHT_MOTOR_INVERTED = false;
+
+
+// CREATING OBJECTS
+motor backLeftMotor = motor( BACK_LEFT_MOTOR_PORT, BACK_LEFT_MOTOR_INVERTED );
+motor frontLeftMotor = motor( FRONT_LEFT_MOTOR_PORT, FRONT_LEFT_MOTOR_INVERTED );
+motor backRightMotor = motor( BACK_RIGHT_MOTOR_PORT, BACK_RIGHT_MOTOR_INVERTED );
+motor frontRightMotor = motor( FRONT_RIGHT_MOTOR_PORT, FRONT_RIGHT_MOTOR_INVERTED );
+
+motor_group leftDriveSmart = motor_group( backLeftMotor, frontLeftMotor );
+motor_group rightDriveSmart = motor_group( backRightMotor, frontRightMotor );
+drivetrain Drivetrain = drivetrain(leftDriveSmart, rightDriveSmart, WHEEL_CIRCUMFERENCE, TRACKWIDTH, WHEELBASE, DISTANCE_UNITS, INTERNAL_GEAR_RATIO );
+
+controller Controller = controller();
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void) {
+  // Nothing to initialize
+}
